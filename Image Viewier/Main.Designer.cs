ef3b750b7e8d62fef4a9d4f011becea502bf16a4
RokerDev <rokerdev91@gmail.com>
2022-07-17 12:04:48 +0200

    add adding watermark to image

--- Image Viewier/Main.Designer.cs
+++ Image Viewier/Main.Designer.cs
@@ -35,6 +35,14 @@
             this.btnClose = new System.Windows.Forms.Button();
             this.pScroll = new System.Windows.Forms.Panel();
             this.sfdSave = new System.Windows.Forms.SaveFileDialog();
+            this.btnAddText = new System.Windows.Forms.Button();
+            this.tbWaterMarkText = new System.Windows.Forms.TextBox();
+            this.cdChangeColor = new System.Windows.Forms.ColorDialog();
+            this.btnChangeTextColor = new System.Windows.Forms.Button();
+            this.fdChangeFont = new System.Windows.Forms.FontDialog();
+            this.btnChangeFont = new System.Windows.Forms.Button();
+            this.lbHelp = new System.Windows.Forms.Label();
+            this.lbHelp1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pbImage)).BeginInit();
             this.pScroll.SuspendLayout();
             this.SuspendLayout();
@@ -47,9 +55,9 @@
             // 
             this.btnOpen.Location = new System.Drawing.Point(12, 12);
             this.btnOpen.Name = "btnOpen";
-            this.btnOpen.Size = new System.Drawing.Size(95, 23);
+            this.btnOpen.Size = new System.Drawing.Size(163, 23);
             this.btnOpen.TabIndex = 0;
-            this.btnOpen.Text = "Open";
+            this.btnOpen.Text = "Import Image";
             this.btnOpen.UseVisualStyleBackColor = true;
             this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
             // 
@@ -57,10 +65,11 @@
             // 
             this.btnSave.Location = new System.Drawing.Point(12, 41);
             this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(95, 23);
+            this.btnSave.Size = new System.Drawing.Size(163, 23);
             this.btnSave.TabIndex = 1;
             this.btnSave.Text = "Save";
             this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Visible = false;
             this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
             // pbImage
@@ -72,15 +81,19 @@
             this.pbImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.pbImage.TabIndex = 2;
             this.pbImage.TabStop = false;
+            this.pbImage.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pbImage_MouseDown);
+            this.pbImage.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pbImage_MouseMove);
+            this.pbImage.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pbImage_MouseUp);
             // 
             // btnClose
             // 
             this.btnClose.Location = new System.Drawing.Point(12, 70);
             this.btnClose.Name = "btnClose";
-            this.btnClose.Size = new System.Drawing.Size(95, 23);
+            this.btnClose.Size = new System.Drawing.Size(163, 23);
             this.btnClose.TabIndex = 3;
             this.btnClose.Text = "Close";
             this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Visible = false;
             this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
             // 
             // pScroll
@@ -91,27 +104,100 @@
             this.pScroll.AutoScroll = true;
             this.pScroll.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.pScroll.Controls.Add(this.pbImage);
-            this.pScroll.Location = new System.Drawing.Point(113, 13);
+            this.pScroll.Location = new System.Drawing.Point(181, 13);
             this.pScroll.Name = "pScroll";
-            this.pScroll.Size = new System.Drawing.Size(682, 581);
+            this.pScroll.Size = new System.Drawing.Size(957, 581);
             this.pScroll.TabIndex = 6;
+            // 
+            // btnAddText
+            // 
+            this.btnAddText.Location = new System.Drawing.Point(12, 99);
+            this.btnAddText.Name = "btnAddText";
+            this.btnAddText.Size = new System.Drawing.Size(163, 23);
+            this.btnAddText.TabIndex = 3;
+            this.btnAddText.Text = "Add Text";
+            this.btnAddText.UseVisualStyleBackColor = true;
+            this.btnAddText.Visible = false;
+            this.btnAddText.Click += new System.EventHandler(this.btnAddText_Click);
+            // 
+            // tbWaterMarkText
+            // 
+            this.tbWaterMarkText.Location = new System.Drawing.Point(12, 164);
+            this.tbWaterMarkText.Name = "tbWaterMarkText";
+            this.tbWaterMarkText.Size = new System.Drawing.Size(163, 20);
+            this.tbWaterMarkText.TabIndex = 3;
+            this.tbWaterMarkText.Visible = false;
+            this.tbWaterMarkText.TextChanged += new System.EventHandler(this.tbWaterMarkText_TextChanged);
+            // 
+            // btnChangeTextColor
+            // 
+            this.btnChangeTextColor.BackColor = System.Drawing.Color.Black;
+            this.btnChangeTextColor.Location = new System.Drawing.Point(12, 190);
+            this.btnChangeTextColor.Name = "btnChangeTextColor";
+            this.btnChangeTextColor.Size = new System.Drawing.Size(24, 23);
+            this.btnChangeTextColor.TabIndex = 7;
+            this.btnChangeTextColor.UseVisualStyleBackColor = false;
+            this.btnChangeTextColor.Visible = false;
+            this.btnChangeTextColor.Click += new System.EventHandler(this.btnChangeTextColor_Click);
+            // 
+            // btnChangeFont
+            // 
+            this.btnChangeFont.Location = new System.Drawing.Point(42, 190);
+            this.btnChangeFont.Name = "btnChangeFont";
+            this.btnChangeFont.Size = new System.Drawing.Size(133, 23);
+            this.btnChangeFont.TabIndex = 8;
+            this.btnChangeFont.Text = "Change Font";
+            this.btnChangeFont.UseVisualStyleBackColor = true;
+            this.btnChangeFont.Visible = false;
+            this.btnChangeFont.Click += new System.EventHandler(this.btnChangeFont_Click);
+            // 
+            // lbHelp
+            // 
+            this.lbHelp.AutoSize = true;
+            this.lbHelp.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lbHelp.Location = new System.Drawing.Point(12, 125);
+            this.lbHelp.Name = "lbHelp";
+            this.lbHelp.Size = new System.Drawing.Size(166, 18);
+            this.lbHelp.TabIndex = 9;
+            this.lbHelp.Text = "Set WaterMark Position";
+            this.lbHelp.Visible = false;
+            // 
+            // lbHelp1
+            // 
+            this.lbHelp1.AutoSize = true;
+            this.lbHelp1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lbHelp1.Location = new System.Drawing.Point(39, 143);
+            this.lbHelp1.Name = "lbHelp1";
+            this.lbHelp1.Size = new System.Drawing.Size(104, 18);
+            this.lbHelp1.TabIndex = 10;
+            this.lbHelp1.Text = "Just Drag Text";
+            this.lbHelp1.Visible = false;
             // 
             // mainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.ClientSize = new System.Drawing.Size(801, 606);
+            this.ClientSize = new System.Drawing.Size(1144, 606);
+            this.Controls.Add(this.lbHelp1);
+            this.Controls.Add(this.lbHelp);
+            this.Controls.Add(this.btnChangeFont);
+            this.Controls.Add(this.btnChangeTextColor);
+            this.Controls.Add(this.tbWaterMarkText);
+            this.Controls.Add(this.btnAddText);
             this.Controls.Add(this.pScroll);
             this.Controls.Add(this.btnClose);
             this.Controls.Add(this.btnSave);
             this.Controls.Add(this.btnOpen);
             this.Name = "mainWindow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Image Viewier";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.mainWindow_FormClosing);
             ((System.ComponentModel.ISupportInitialize)(this.pbImage)).EndInit();
             this.pScroll.ResumeLayout(false);
             this.pScroll.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -124,6 +210,14 @@
         private System.Windows.Forms.Button btnClose;
         private System.Windows.Forms.Panel pScroll;
         private System.Windows.Forms.SaveFileDialog sfdSave;
+        private System.Windows.Forms.Button btnAddText;
+        private System.Windows.Forms.TextBox tbWaterMarkText;
+        private System.Windows.Forms.ColorDialog cdChangeColor;
+        private System.Windows.Forms.Button btnChangeTextColor;
+        private System.Windows.Forms.FontDialog fdChangeFont;
+        private System.Windows.Forms.Button btnChangeFont;
+        private System.Windows.Forms.Label lbHelp;
+        private System.Windows.Forms.Label lbHelp1;
     }
 }
 
--- Image Viewier/Main.cs
+++ Image Viewier/Main.cs
@@ -1,61 +1,230 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Drawing.Drawing2D;
+using System.Drawing.Text;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Image_Viewier
 {
     public partial class mainWindow : Form
     {
+        private int _X;
+        private int _Y;
+        private int _fontSize;
+        private bool _wantAdd;
+        private Font _font;
+        private Color _fontColor;
+        private string _waterMarkText;
+        private Brush _brush;
+        private Bitmap _bitmap;
+        private Graphics _graphics;
+
+
         public mainWindow()
         {
             InitializeComponent();
+            _fontSize = 20;
+            _wantAdd = false;
+            fdChangeFont.Font = new Font("Arial", _fontSize, FontStyle.Regular);
+            _font = fdChangeFont.Font;
+            _fontColor = Color.FromKnownColor(KnownColor.Black);
+            _waterMarkText = "";                        
+        }
+
+        private void ResetApp()
+        {
+            HideOptionButtons();
+            HideImageOptionButtons();
+            ClearPictureBox();
         }
 
         private void ClearPictureBox()
         {
             if (pbImage.Image != null)
                 pbImage.Image.Dispose();
+            if (_bitmap != null)
+                _bitmap.Dispose();
+            if (_graphics != null)
+                _graphics.Dispose();
+            if (_brush != null)
+                _brush.Dispose();
+
             pbImage.Image = null;
+        }
+
+        private void RefreshPictureBox()
+        {
+            if (!_wantAdd)
+                return;
+
+            ClearPictureBox();
+            _bitmap = new Bitmap(ofdOpen.FileName);
+            pbImage.Image = (Image)_bitmap;
+            AddTextToImage();
+        }
+
+        private void RefreshChanges()
+        {
+            _wantAdd = true;
+            RefreshPictureBox();
+            _wantAdd = false;
         }
 
         private void AddTextToImage()
         {
 
+            _graphics = Graphics.FromImage((Bitmap)pbImage.Image);
+            _graphics.TextRenderingHint = TextRenderingHint.AntiAliasGridFit;
+
+            _brush = new SolidBrush(_fontColor);
+
+            Point point = new Point(_X, _Y);
+
+            _graphics.DrawString(_waterMarkText, _font, _brush, point);            
+
+            pbImage.Image = (Image)pbImage.Image;            
         }
 
         private void btnOpen_Click(object sender, EventArgs e)
         {
-            ofdOpen.ShowDialog();
+            try
+            {
+                ofdOpen.Filter = "" +
+                        "Image Files (*.png)|*.png|" +
+                        "Image Files (*.jpg)|*.jpg|" +
+                        "Image Files (*.gif)|*.gif|" +
+                        "All Files (*.*)|*.*";
 
-            if (!File.Exists(ofdOpen.FileName))
-                return;
+                ofdOpen.ShowDialog();
 
-            pScroll.Controls.Add(pbImage);
-            Bitmap bitmap = new Bitmap(ofdOpen.FileName);
-            pbImage.Image = (Image)bitmap;            
+                if (!File.Exists(ofdOpen.FileName))
+                    return;
+
+                pScroll.Controls.Add(pbImage);
+                Bitmap bitmap = new Bitmap(ofdOpen.FileName);
+                if (bitmap != null)
+                {
+                    pbImage.Height = bitmap.Height;
+                    pbImage.Width = bitmap.Width;
+                    pbImage.Load(ofdOpen.FileName);
+                    bitmap.Dispose();
+                    ShowOptionButtons();
+                }                               
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Wrong File. Please try Again");
+            }
+        }
+
+        private void HideOptionButtons()
+        {
+            btnSave.Visible = false;
+            btnClose.Visible = false;
+            btnAddText.Visible = false;
+            btnOpen.Visible = true;
+        }
+
+        private void ShowOptionButtons()
+        {
+            btnSave.Visible = true;
+            btnClose.Visible = true;
+            btnAddText.Visible = true;
+            btnOpen.Visible = false;
+        }
+
+        private void ShowImageOptionButtons()
+        {
+            tbWaterMarkText.Visible = true;
+            btnChangeFont.Visible = true;
+            btnChangeTextColor.Visible = true;
+            btnAddText.Visible = false;
+            lbHelp.Visible = true;
+            lbHelp1.Visible = true;
+        }
+
+        private void HideImageOptionButtons()
+        {
+            tbWaterMarkText.Visible = false;
+            btnChangeFont.Visible = false;
+            btnChangeTextColor.Visible = false;
+            btnAddText.Visible = false;
+            lbHelp.Visible = false;
+            lbHelp1.Visible = false;
         }
 
         private void btnClose_Click(object sender, EventArgs e)
         {
-            ClearPictureBox();
+            ResetApp();
         }
 
         private void btnSave_Click(object sender, EventArgs e)
         {
+            sfdSave.DefaultExt = "png";
+            sfdSave.Filter = "Image Files (*.png)|*.png|All Files (*.*)|*.*";
+            sfdSave.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
             sfdSave.ShowDialog();
 
-            if (sfdSave.CheckPathExists)
-                return;
-
             pbImage.Image.Save(sfdSave.FileName);
+            ResetApp();
+        }
+
+        private void pbImage_MouseDown(object sender, MouseEventArgs e)
+        {
+            _wantAdd = true;
+        }
+
+        private void pbImage_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_wantAdd)
+            {
+                _X = e.X;
+                _Y = e.Y;
+                RefreshPictureBox();
+            }
+        }
+
+        private void btnAddText_Click(object sender, EventArgs e)
+        {
+            _X = 0;
+            _Y = 0;
+            tbWaterMarkText.Text = "Default Text";
+            ShowImageOptionButtons();
+        }
+
+        private void pbImage_MouseUp(object sender, MouseEventArgs e)
+        {
+            _wantAdd = false;
+        }
+
+        private void btnChangeTextColor_Click(object sender, EventArgs e)
+        {
+            cdChangeColor.ShowDialog();
+            btnChangeTextColor.BackColor = cdChangeColor.Color;
+            _fontColor = cdChangeColor.Color;
+            
+            _brush.Dispose();
+            _brush = new SolidBrush(_fontColor);
+            RefreshChanges();
+        }
+
+        private void btnChangeFont_Click(object sender, EventArgs e)
+        {
+            fdChangeFont.ShowDialog();
+            btnChangeFont.Text = fdChangeFont.Font.Name;
+            _font = fdChangeFont.Font;
+            RefreshChanges();
+        }
+
+        private void tbWaterMarkText_TextChanged(object sender, EventArgs e)
+        {
+            _waterMarkText = tbWaterMarkText.Text;
+            RefreshChanges();
+        }
+
+        private void mainWindow_FormClosing(object sender, FormClosingEventArgs e)
+        {
             ClearPictureBox();
         }
     }
